cb4d83f6251a26e8918d15ddf07a8111c54f5477
duyminh0510 <mduy2186@gmail.com>
2025-05-06 10:00:27 +0700

    update

---
+++ src/main/java/graduation/datn/models/Role.java
@@ -0,0 +1,32 @@
+package graduation.datn.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roleId")
+    private Integer roleId;
+
+    @Column(name = "name")
+    private String name;
+
+}
